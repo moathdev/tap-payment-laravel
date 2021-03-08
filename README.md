5b360db9c1fb28833071b90e495d10efaeb169b3
Moathdev <moad101@gmail.com>
2021-03-08 23:18:11 +0300

    I've forgotten how it felt before the world fell at our feet

--- README.md
+++ README.md
@@ -41,13 +41,69 @@ Run `php artisan vendor:publish --provider="Moathdev\Tap\TapServiceProvider"`
 
 * `getCharge()` - Retrieves the details of a charge that has previously been created. Supply the unique charge id that was returned from your  previous request, and Tap will return the corresponding charge information. The same information is returned when creating the charge.
 * `CreateCharge()` - To charge a credit card or debit card (Knet, mada, Visa, MasterCard) or an existing authorized transactions, you create a charge request. If your API key is in test mode, the card won't actually be charged, though everything else will occur as if in live mode.
+* `UpdateCharge()` - Updates the specified charge by setting the values of the parameters passed. Any parameters not provided will be left unchanged.
+* `getAllCharges()` - Returns a list of charges you’ve previously created. The charges are returned in sorted order, with the most recent charges appearing first.
 
 ## Examples
 
-Returns a collection of the most recent Tweets posted by the user indicated by the screen_name or user_id parameters.
+- Retrieve a Charge
 ```php
 Route::get('/getCharge/{charge_id}', function($charge_id)
 {
-	return Tap::getCharge(['charge_id' => $charge_id]);
+	$res = Tap::getCharge(['charge_id' => $charge_id]);
+
+    dd($res);
 });
 ```
+
+- Create a Charge
+```php
+Route::get('/CreateCharge', function()
+{
+	    $res  = Tap::CreateCharge([
+            'amount'=> 1,
+            'currency' => 'SAR',
+            'threeDSecure' => true,
+            'save_card' => false,
+            'description' => 'Test Description',
+            'statement_descriptor' => 'Sample',
+            'metadata' => [
+                'udf1' => 'test 1',
+                'udf2' => 'test 2',
+            ],
+            'reference' => [
+                'transaction' => 'txn_0001',
+                'order' => 'ord_0001',
+            ],
+            'receipt' => [
+                'email' => false,
+                'sms' => false,
+            ],
+            'customer' => [
+                'first_name' => "test",
+                'middle_name' => "test",
+                'last_name' => "test",
+                'email' => "test@test.com",
+                'phone' => [
+                    'country_code' => "965",
+                    'number' => "50000000",
+                ],
+            ],
+            'merchant' => [
+                'id' => ''
+            ],
+            'source' => [
+                'id' => 'src_all',
+            ],
+            'post' => [
+                'url' => 'http://your_website.com/post_url'
+            ],
+            'redirect' => [
+                'url' => 'http://your_website.com/post_url'
+            ]
+        ]);
+        
+        dd($res);
+});
+```
+
--- src/Traits/ChargeTrait.php
+++ src/Traits/ChargeTrait.php
@@ -8,8 +8,8 @@ trait ChargeTrait
 {
     /**
      * Retrieves the details of a charge that has previously been created. Supply the unique charge id that was returned from your
-     * previous request, and Tap will return the corresponding charge information. The same information is returned when creating
-     * the charge.
+     * previous request, and Tap will return the corresponding charge information. The same information is returned when creating the charge.
+     *
      * @see https://www.tap.company/sa/en/developers > Carages > Retrieve a Charge
      *
      *  Parameters :
@@ -31,6 +31,7 @@ trait ChargeTrait
      * To charge a credit card or debit card (Knet, mada, Visa, MasterCard) or an existing authorized transactions, you create a charge request. If your
      * API key is in test mode, the card won't actually be charged, though everything else will occur as if in live mode.
      * (Tap assumes that the charge would have completed successfully).
+     *
      * @see https://www.tap.company/sa/en/developers > Carages > Create a Charge
      *
      *  Parameters :
@@ -40,16 +41,16 @@ trait ChargeTrait
      * - save_card | boolean | optional
      * - description | string | optional
      * - statement_descriptor | string | optional
-     * - metadata| object | optional
-     * - reference| object | optional
-     * - receipt| object | optional
-     * - customer| object | required
-     * - source| object | required
-     * - airline| object | optional
-     * - destinations| object | optional
-     * - application| object | optional
-     * - redirect | object | optional
-     * - merchant | object | optional
+     * - metadata| array[string] | optional
+     * - reference| array[string] | optional
+     * - receipt| array[string] | optional
+     * - customer| array[string] | required
+     * - source| array[string] | required
+     * - airline| array[string] | optional
+     * - destinations| array[string] | optional
+     * - application| array[string] | optional
+     * - redirect | array[string] | optional
+     * - merchant | array[string] | optional
      * @param array $parameters
      * @return object
      */
@@ -64,5 +65,55 @@ trait ChargeTrait
         }
 
         return $this->post('/charges/', $parameters);
+    }
+
+    /**
+     * Updates the specified charge by setting the values of the parameters passed. Any parameters not provided will be left unchanged.
+     * This request accepts only the description, metadata and receipt arguments.
+     *
+     * @see https://www.tap.company/sa/en/developers > Carages > Update a Charge
+     *
+     *  Parameters :
+     * - description | string | optional
+     * - receipt | string | optional
+     * - metadata |  array[string] | optional
+     * - airline | array[string] | optional
+     * - customers | array[string] | optional
+     * - charges | array[string] | optional
+     * - starting_after | array[string] | optional
+     * - payout | array[string] | optional
+     * @param array $parameters
+     * @return object
+     */
+    public function UpdateCharge($parameters = [])
+    {
+        if (!array_key_exists('charge_id', $parameters)) {
+            throw new BadMethodCallException('Parameter required missing : charge_id');
+        }
+
+        return $this->post('/charges/', $parameters);
+    }
+
+    /**
+     * Returns a list of charges you’ve previously created.
+     * The charges are returned in sorted order, with the most recent charges appearing first.
+     *
+     * @see https://www.tap.company/sa/en/developers > Carages > List all Charges
+     *
+     *  Parameters :
+     * - period | array[string] | optional
+     * - status | string | optional
+     * - sources |  array[string] | optional
+     * - payment_methods | array[string] | optional
+     * - customers | array[string] | optional
+     * - charges | array[string] | optional
+     * - starting_after | array[string] | optional
+     * - limit | integer | optional
+     * @param array $parameters
+     * @return object
+     */
+    public function getAllCharges($parameters = [])
+    {
+        return $this->post('/charges/list/', $parameters);
     }
 }
